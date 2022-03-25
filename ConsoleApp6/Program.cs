e0e55308412e8891f4b50dcdbe9e924e9d5484af
3110680 <3110680@TE-GDA-D-D26F04>
2022-03-25 14:23:48 +0100

    Dodaj pliki projektów.

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Napisz wiadomość do zakodowania");
+string wiadomość = Console.ReadLine();
+char[] achar = wiadomość.ToCharArray();
+int x;
+Console.WriteLine("O ile chcesz przesunac?");
+x = Int32.Parse(Console.ReadLine());
+for (int i = 0; i < achar.Length; i++)
+{
+    achar[i] = (char)((achar[i] + x));
+    if (achar[i] > 90)
+    {
+        achar[i] = (char)((char)achar[i] - 26);
+    }
+
+}
+string wiadomość2 = new string(achar);
+Console.WriteLine("Wiadomość zaszyfrowana to:");
+Console.WriteLine(wiadomość2);
